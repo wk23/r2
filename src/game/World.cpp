90378a591f80406541716f98eeabfcea215d76c0
unknown <Administrator@.(none)>
2009-07-31 01:42:13 +0300

    online fix

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -1396,7 +1396,7 @@ void World::SetInitialWorldSettings()
     sprintf( isoDate, "%04d-%02d-%02d %02d:%02d:%02d",
         local.tm_year+1900, local.tm_mon+1, local.tm_mday, local.tm_hour, local.tm_min, local.tm_sec);
 
-    loginDatabase.PExecute("INSERT INTO uptime (online, realmid, starttime, startstring, uptime) VALUES('%u', " UI64FMTD ", '%s', 0)",
+    loginDatabase.PExecute("INSERT INTO uptime (online, realmid, starttime, startstring, uptime) VALUES('%u', '%u', " UI64FMTD ", '%s', 0)",
         sWorld.GetActiveSessionCount(), realmID, uint64(m_startTime), isoDate);
 
     m_timers[WUPDATE_OBJECTS].SetInterval(0);
