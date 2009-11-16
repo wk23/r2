fa182c9e4f50b1e2778b537ea624946e977d8602
unknown <Administrator@.(none)>
2009-11-16 08:49:15 +0200

    faction attack?

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -517,6 +517,13 @@ uint32 Unit::DealDamage(Unit *pVictim, uint32 damage, CleanDamage const* cleanDa
             return 0;
     }
 
+    if(GetTypeId() == TYPEID_PLAYER && pVictim->GetTypeId() == TYPEID_PLAYER)
+    if (((Player*)this)->getFaction() != ((Player*)this)->getFactionForRace(((Player*)this)->getRace()) && !((Player*)this)->isGameMaster())
+    {
+        sLog.outError("Player (aGUID: %u) attack (vGUID: %u) at map %u, possible cheat faction change", GetGUID(), pVictim->GetGUID(), GetMapId());
+        return 0;
+    }
+
     // remove affects from victim (including from 0 damage and DoTs)
     if(pVictim != this)
         pVictim->RemoveSpellsCausingAura(SPELL_AURA_MOD_STEALTH);
