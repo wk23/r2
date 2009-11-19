cd42c9bf9dff1499170bd79cc79ce6385fbeccae
unknown <Administrator@.(none)>
2009-11-19 14:34:50 +0200

    //assert duel crash

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4192,7 +4192,7 @@ void Unit::AddGameObject(GameObject* gameObj)
 
 void Unit::RemoveGameObject(GameObject* gameObj, bool del)
 {
-    assert(gameObj && gameObj->GetOwnerGUID()==GetGUID());
+    //assert(gameObj && gameObj->GetOwnerGUID()==GetGUID());
 
     gameObj->SetOwnerGUID(0);
 
